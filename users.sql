ce59219633c6b1ea96e862292f365fa10d8a5a9c
Your Name <johnandresyap510@gmail.com>
2025-03-01 21:31:49 +0800

    Added 'profile_picture' column

--- users.sql
+++ users.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 26, 2025 at 05:10 AM
+-- Generation Time: Mar 01, 2025 at 02:30 PM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -32,6 +32,7 @@ CREATE TABLE `users` (
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
   `user_type` enum('admin','worker','client') NOT NULL,
+  `profile_picture` varchar(200) DEFAULT NULL,
   `first_name` varchar(50) NOT NULL,
   `last_name` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
@@ -43,8 +44,8 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `username`, `password`, `user_type`, `first_name`, `last_name`, `email`, `phone_number`, `application_status`) VALUES
-(12, 'apotato369', '$2y$10$LBO8bWtTqMrAikit3lZUZ.vWevDmpSIa5F2Lg7a8x6yFV84H0uke2', 'admin', 'John Andre', 'Yap', 'apotato369@gmail.com', '09150443019', 'approved');
+INSERT INTO `users` (`user_id`, `username`, `password`, `user_type`, `profile_picture`, `first_name`, `last_name`, `email`, `phone_number`, `application_status`) VALUES
+(12, 'apotato369', '$2y$10$LBO8bWtTqMrAikit3lZUZ.vWevDmpSIa5F2Lg7a8x6yFV84H0uke2', 'admin', NULL, 'John Andre', 'Yap', 'apotato369@gmail.com', '09150443019', 'approved');
 
 --
 -- Indexes for dumped tables
