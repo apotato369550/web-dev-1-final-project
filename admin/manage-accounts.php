bfd1bbd9e81dda585ec1fffd9442f28f7235093e
Your Name <johnandresyap510@gmail.com>
2025-03-04 21:47:55 +0800

    Started work on displaying different accounts to be managed

--- admin/manage-accounts.php
+++ admin/manage-accounts.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="admin.css">
     <title>Manage Announcements - Cebu Best Value Trading</title>
 </head>
 <body>
@@ -12,6 +13,8 @@
         header("Location: ../index.php?error=unauthorized");
         exit();
     }
+
+    include "admin-navbar.php";
     ?>
     <div class="manage-accounts-container">
         <div class="manage-accounts">
@@ -20,7 +23,7 @@
             </div>
             <div class="manage-accounts-interface">
                 <?php 
-                include "../includes/dbh.inc.php"
+                include "../includes/dbh.inc.php";
 
                 $sessionUserId = $_SESSION["user_id"];
                 $sessionUsername = $_SESSION["username"];
@@ -32,20 +35,19 @@
                     echo "Error when preparing SQL statement.";
                 }
             
-                mysqli_stmt_bind_param($stmt);
-            
                 if (!mysqli_stmt_execute($stmt)) {
                     echo "Error while executing SQL statement";
                 }
             
                 $results = mysqli_stmt_get_result($stmt);
 
-                while ($row = mysqli_fetch_assoc($result)) {
+                while ($row = mysqli_fetch_assoc($results)) {
                     $userId = $row["user_id"];
                     $username = $row["username"];
                     if ($sessionUserId === $userId && $sessionUsername === $username) {
                         continue;
                     }
+                    $userId = $row["user_id"];
                     $userType = $row["user_type"];
                     $firstName = $row["first_name"];
                     $lastName = $row["last_name"];
@@ -54,8 +56,36 @@
                     $profilePicture = $row["profile_picture"];
                     $applicationStatus = $row["application_status"];
 
+                    if (empty($profilePicture)) {
+                        $profilePicture = "../assets/default_pfp.png";
+                    }
+
                     ?>
-                    <div>Username: <?php echo $username ?></div>
+                    <div class="account">
+                        <div class="account-profile-picture">
+                            <img src="<?php echo $profilePicture ?>" alt="">
+                        </div>
+                        <div class="account-info">
+                            <h1>User #<?php echo $userId ?>: <?php echo $username ?></h1>
+                            <h1>Account Type: <?php echo $userType ?> </h1>
+                            <p>Full Name: <?php echo $lastName.", ".$firstName ?></p>
+                            <p>Email: <?php echo $email ?> </p>
+                            <p>Phone Number: <?php echo $phoneNumber ?> </p>
+                        </div>
+                        <div class="account-options">
+                            <form action="../includes/update-account-status.inc.php" method="post">
+                                <input type="number" value="<?php echo $userId; ?>" style="display: none;">
+                                
+                                <p>Status: </p>
+                                <select name="account-status">
+                                    <option value="active" <?php if ($applicationStatus === "accepted") { echo "selected"; } ?>>Active</option>
+                                    <option value="disabled" <?php if ($applicationStatus === "rejected") { echo "selected"; } ?>>Disabled</option>
+                                    <option value="inactive" <?php if ($applicationStatus === "pending") { echo "selected"; } ?>>Inactive</option>
+                                </select>
+                                <button type="submit" name="update-account-status">Update</button>
+                            </form>
+                        </div>
+                    </div>
                     <?php
                 }
 
