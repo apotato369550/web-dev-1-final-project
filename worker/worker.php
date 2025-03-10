4a5211fe39ffb504d5e7a327efa602fa5b9a484f
John Andre Yap <johnandresyap510@gmail.com>
2025-03-10 22:23:01 +0800

    Added worker navbar

--- worker/worker.php
+++ worker/worker.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="worker.css">
     <title>Worker - Cebu Best Value Trading</title>
 </head>
 <body>
@@ -12,6 +13,8 @@
         header("Location: ../index.php?error=unauthorized");
         exit();
     }
+
+    include "worker-navbar.php";
     ?>
     <h1>Welcome Worker!</h1>
     <a href="../includes/logout.inc.php">Logout</a>
