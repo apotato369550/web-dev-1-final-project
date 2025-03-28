aa060f624188283153c427fc01fd38d4f71f1f79
apotato369550 <65766356+apotato369550@users.noreply.github.com>
2025-03-28 14:10:45 +0800

    Add sidebar

---
+++ admin/admin-sidebar.php
@@ -0,0 +1,17 @@
+<nav>
+    <div class="sidebar-nav-title">
+        <h1>Admin Controls</h1>
+    </div>
+    <div class="sidebar-container">
+        <div class="sidebar-content">
+            <ul>
+                <li><a href="manage-accounts.php">Manage Accounts</a></li>
+                <li><a href="manage-jobs.php">Manage Jobs</a></li>
+                <li><a href="manage-requests.php">Manage Client Requests</a></li>
+                <li><a href="manage-announcements.php">Manage Announcements</a></li>
+                <li><a href="#">Manage Schedule</a></li>
+                <li><a href="#">Manage Quotations</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
